e83d1a655440075b3cfa38a81f061e574c560691
pnjia <panjiangkasaputra67@gmail.com>
2025-11-24 15:24:50 +0700

    Menghapus styles yang tidak terpakai

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { StyleSheet } from "react-native";
 
 const authLayout = () => {
   return <Stack screenOptions={{ headerShown: false }} />;
