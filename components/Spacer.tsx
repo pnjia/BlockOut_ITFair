af15ab7a2aadd78ea0301be9da12ba5630343cfd
pnjia <panjiangkasaputra67@gmail.com>
2025-11-24 15:15:04 +0700

    Menambahkan spacer untuk jarak antar komponen

---
+++ components/Spacer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, View, ViewStyle } from "react-native";
+
+type SpacerProps = {
+  width?: number | string;
+  height?: number | string;
+};
+
+const Spacer = ({ width = "100%", height = 40 }: SpacerProps) => {
+  return <View style={{ width, height } as ViewStyle} />;
+};
+
+export default Spacer;
+
+const styles = StyleSheet.create({});
