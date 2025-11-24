5fe404ddca6224961675cb7b02672d19e353db54
pnjia <panjiangkasaputra67@gmail.com>
2025-11-24 15:16:28 +0700

    Isinya adalah seperti color, ukurang font, font family, ll

---
+++ constants/theme.ts
@@ -0,0 +1,18 @@
+export const GlobalStyles = {
+  fontRegular: "jersey10",
+};
+
+export const Colors = {
+  primary: "#222831",
+  secondary: "#393E46",
+  tertiary: "#00ADB5",
+  quarternary: "#EEEEEE",
+};
+// Use numeric font sizes for React Native
+export const FontSizes = {
+  appTitle: 48,
+  h1: 40,
+  h2: 30,
+  h3: 20,
+  body: 16,
+};
