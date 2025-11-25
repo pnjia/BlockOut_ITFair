0cbcfd0ef11ede22dc78e6a18f618d0646efe6a4
pnjia <panjiangkasaputra67@gmail.com>
2025-11-25 20:01:33 +0700

    Menambahkan rute baru account

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout() {
         <Stack.Screen name="test" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        <Stack.Screen name="(account)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
