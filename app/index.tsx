444085ab1e238d44d35de7522fa48b9d2cecee95
pnjia <panjiangkasaputra67@gmail.com>
2025-11-25 16:39:33 +0700

    Membuat halaman first look (belum dengan style)

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,22 @@
 import TextStyle from "@/components/TextStyle";
 import ViewStyle from "@/components/ViewStyle";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { useEffect } from "react";
 
 export default function Index() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/(auth)/signin"); // arahkan ke halaman login
+    }, 2500); // 2.5 detik
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <ViewStyle>
       <TextStyle>Percobaan</TextStyle>
       <Link href="/(auth)/signin">Signin</Link>
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/test">Test</Link>
     </ViewStyle>
   );
 }
