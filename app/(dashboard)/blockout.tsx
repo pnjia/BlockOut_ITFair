67b21ed56f87e0c987ae7e0aad22d3287ae337d5
pnjia <panjiangkasaputra67@gmail.com>
2025-11-25 20:02:18 +0700

    Tampilan blockout (Belum selesai bagian appnya)

--- app/(dashboard)/blockout.tsx
+++ app/(dashboard)/blockout.tsx
@@ -1,13 +1,47 @@
+import InputStyle from "@/components/InputStyle";
+import Spacer from "@/components/Spacer";
 import TextStyle from "@/components/TextStyle";
 import ViewStyle from "@/components/ViewStyle";
+import { Colors } from "@/constants/theme";
+import { Entypo } from "@expo/vector-icons";
 import React from "react";
+import { Pressable, View } from "react-native";
 
 const Blockout = () => {
   return (
-    <ViewStyle>
-      <TextStyle variant="h3" color="quarternary">
-        blockout
-      </TextStyle>
+    <ViewStyle style={{ justifyContent: "flex-start", paddingTop: 60 }}>
+      <View style={{ width: "85%", alignItems: "center" }}>
+        <TextStyle variant="h1" color="tertiary">
+          Select Apps to Block
+        </TextStyle>
+
+        <Spacer height={20} />
+
+        <View style={{ position: "relative", width: "100%" }}>
+          <InputStyle
+            placeholder="Search for an app"
+            style={{
+              width: "100%",
+              backgroundColor: "#393E46",
+              borderWidth: 0,
+            }}
+          />
+
+          <Pressable
+            style={{
+              position: "absolute",
+              right: 10,
+              top: 8,
+            }}
+          >
+            <Entypo
+              name="magnifying-glass"
+              size={24}
+              color={Colors.quarternary}
+            />
+          </Pressable>
+        </View>
+      </View>
     </ViewStyle>
   );
 };
