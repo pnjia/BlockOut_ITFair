67d99335dee9b1db2ecd8e50b8c68d2e4fe5f18b
pnjia <panjiangkasaputra67@gmail.com>
2025-11-26 21:10:37 +0700

    Animasi fade

---
+++ app/(notification)/_layout.tsx
@@ -0,0 +1,22 @@
+import { Colors } from "@/constants/theme";
+import { Stack } from "expo-router";
+import React from "react";
+
+const NotificationLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: Colors.primary,
+        },
+        navigationBarColor: Colors.primary,
+        animation: "fade",
+        animationDuration: 50,
+        freezeOnBlur: true,
+      }}
+    />
+  );
+};
+
+export default NotificationLayout;
