ce5453b0a242f18f3f7378241caff514dad3669b
pnjia <panjiangkasaputra67@gmail.com>
2025-11-28 08:02:55 +0700

    refactor: wrap main content in SafeAreaView for improved layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
 import TextStyle from "@/components/TextStyle";
 import ViewStyle from "@/components/ViewStyle";
+import { Colors } from "@/constants/theme";
 import { Link, router } from "expo-router";
 import { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   useEffect(() => {
@@ -12,10 +14,15 @@ export default function Index() {
     return () => clearTimeout(timer);
   }, []);
   return (
-    <ViewStyle>
-      <TextStyle>Percobaan</TextStyle>
-      <Link href="/(auth)/signin">Signin</Link>
-      <Link href="/(auth)/signup">Signup</Link>
-    </ViewStyle>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: Colors.primary }}
+      edges={["top", "bottom"]}
+    >
+      <ViewStyle>
+        <TextStyle>Percobaan</TextStyle>
+        <Link href="/(auth)/signin">Signin</Link>
+        <Link href="/(auth)/signup">Signup</Link>
+      </ViewStyle>
+    </SafeAreaView>
   );
 }
